51907391a8d34f7d54ef048dce5f45b4ac57a069
Alvaro.Soler <6766722@alu.murciaeduca.es>
2018-10-08 16:11:33 +0200

    getNombre

--- common/models/juego.js
+++ common/models/juego.js
@@ -2,4 +2,12 @@
 
 module.exports = function(Juego) {
 
+/**
+ * Devuelve el nombre de un juego
+ * @param {Function(Error, string)} callback
+ */
+Juego.prototype.getNombre = function(callback) {
+  // TODO
+  callback(null, this.nombre);
+};
 };
